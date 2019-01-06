425d01dc146f2013caa1987c4319984e619b2949
Patrick Keenan <sirberus.io@gmail.com>
2019-01-06 15:31:18 -0800

    new plugins

---
+++ admin/src/plugins/context-menu.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import VueSimpleContextMenu from 'vue-simple-context-menu'
+import 'vue-simple-context-menu/dist/vue-simple-context-menu.css'
+
+Vue.component('vue-simple-context-menu', VueSimpleContextMenu)
