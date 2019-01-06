fbef0df54d7b3e863818c5bd37a825125d407cdf
Patrick Keenan <sirberus.io@gmail.com>
2019-01-06 15:31:27 -0800

    updates; new API

--- admin/src/plugins/primitives.js
+++ admin/src/plugins/primitives.js
@@ -1,9 +1,76 @@
-import { registerPrimitive } from "vue-primitives";
+import { registerPrimitives } from "vue-primitives";
 import Vue from "vue";
 
-registerPrimitive(Vue, "div", "box", ["flex", "column"]);
-registerPrimitive(Vue, "span", "txt");
-registerPrimitive(Vue, "span", "mdi");
-registerPrimitive(Vue, "div", "row", ["flex"]);
-registerPrimitive(Vue, "div", "column", ["flex", "column"]);
-registerPrimitive(Vue, "div", "wrapper", ["flex", "center"]);
+const adaptiveTextColor = {
+  primary: ({store}) => store.getters.dark ? 'text-grey-light' : 'text-white',
+  secondary: ({store}) => store.getters.dark ? 'text-grey-darkest hover:text-black' : 'text-grey-darkest',
+  accent: ({store}) => store.getters.dark ? 'text-grey-dark' : 'text-grey-light',
+  accentReactive: ({store}) => store.getters.dark ? 'text-grey-dark hover:text-grey-light' : 'text-grey-light hover:text-white'
+}
+
+const adaptiveBgColor = {
+  primary: ({store}) => store.getters.dark ? 'bg-black' : 'bg-grey-dark',
+  secondary: ({store}) => store.getters.dark ? 'text-grey-light' : 'text-grey-darkest',
+  accent: ({store}) => store.getters.dark ? 'text-grey-dark' : 'text-grey-light'
+}
+
+registerPrimitives(Vue, {
+  Container: { tag: "div" },
+  Txt: { tag: "span" },
+  Mdi: { tag: "span" },
+  Row: { tag: "div", classes: 'flex' },
+  Column: { tag: "div", classes: 'flex flex-col' },
+  
+  TxtPrimary: { 
+    tag: "span", 
+    classes: 'darkmode-transition',
+    conditionals: [adaptiveTextColor.primary]
+  },
+  TxtSecondary: { 
+    tag: "span", 
+    classes: 'darkmode-transition',
+    conditionals: [adaptiveTextColor.secondary]
+  },
+  TxtAccent: { 
+    tag: "span", 
+    classes: 'darkmode-transition',
+    conditionals: [adaptiveTextColor.accent]
+  },
+
+  IconPrimaryStatic: { 
+    tag: "span", 
+    classes: 'mdi darkmode-transition',
+    conditionals: [adaptiveTextColor.primary]
+  },
+  IconAccent: { 
+    tag: "span", 
+    classes: 'mdi darkmode-transition cursor-pointer',
+    conditionals: [adaptiveTextColor.accentReactive]
+  },
+  IconAccent: { 
+    tag: "span", 
+    classes: 'mdi darkmode-transition cursor-pointer',
+    conditionals: [adaptiveTextColor.accentReactive]
+  },
+
+  BoxPrimary: { 
+    tag: "div", 
+    classes: 'darkmode-transition',
+    conditionals: [adaptiveBgColor.primary]
+  },
+
+  ModalBtn: {
+    tag: 'button',
+    classes: 'rounded py-2 px-4 flex-no-grow darkmode-transition font-semibold',
+    conditionals: [
+      ({store}) => store.getters.dark ? 'bg-grey-dark text-black' : 'bg-grey-light text-grey-darkest'
+    ]
+  },
+  LoginBtn: {
+    tag: 'button',
+    classes: 'rounded py-2 w-24 px-4 flex-no-grow darkmode-transition font-semibold',
+    conditionals: [
+      ({store}) => store.getters.dark ? 'bg-grey-darkest text-white' : 'bg-grey-light text-grey-darkest'
+    ]
+  }
+})
