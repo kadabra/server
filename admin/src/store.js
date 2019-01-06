42c51f159c82018ff808a7e091289c0d4fbc83a7
Patrick Keenan <sirberus.io@gmail.com>
2019-01-06 15:30:50 -0800

    updates

--- admin/src/store.js
+++ admin/src/store.js
@@ -6,23 +6,10 @@ Vue.use(SimpleVuex)
 export default SimpleVuex.Store({
   state: {
     dark: true,
-    menu: false,
-    width: 0,
-    height: 0,
-
-    connections: [
-      {
-        name: 'Local',
-        host: 'http://localhost:7777'
-      }
-    ],
-
+    modal: false,
+    route: 'loading',
+    loggedIn: false,
+    url: webpackHotUpdate ? 'localhost:7777' : window.location.href,
     endpoint: null
-  },
-  mutations: {
-    'replace-connection'(state, payload) {
-      const { index, connection } = payload
-      state.connections.splice(index, 1, connection)
-    }
   }
 })
