53083cb08fa3940d547bbc2bb76309cabede47da
Patrick Keenan <sirberus.io@gmail.com>
2019-01-06 18:49:22 -0800

    add disabled as prop

--- admin/src/components/TextField.vue
+++ admin/src/components/TextField.vue
@@ -2,8 +2,11 @@
   <column field mb-6>
     <txt-accent v-if="name" text-xs uppercase>{{name}}</txt-accent>
     <input :value="text" @input="e => $emit('input', e.target.value)"
-      :type="type" class="mt-2 w-full p-1 darkmode-transition" :placeholder="placeholder"
+      :disabled="disabled"
+      :type="type" class="mt-2 w-full p-1 darkmode-transition" 
+      :placeholder="placeholder"
       :class="{
+        'opacity-50': disabled,
         'bg-grey-dark text-black': $store.getters.dark,
         'bg-grey-light text-grey-darkest': !$store.getters.dark
       }"
@@ -14,13 +17,14 @@
 <script>
 export default {
   model: {
-    prop: 'text'
+    prop: 'text',
   },
   props: {
     text: {
       type: String,
       required: true
     },
+    disabled: Boolean,
     name: String,
     type: {
       type: String,
