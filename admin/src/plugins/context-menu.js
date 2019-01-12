788154029fe6c43b9890e719d7e2d14b00634bb2
Patrick Keenan <sirberus.io@gmail.com>
2019-01-12 03:34:56 -0800

    move admin to separate repository

--- admin/src/plugins/context-menu.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import VueSimpleContextMenu from 'vue-simple-context-menu'
-import 'vue-simple-context-menu/dist/vue-simple-context-menu.css'
-
-Vue.component('vue-simple-context-menu', VueSimpleContextMenu)
--- admin/src/plugins/vue-feathers.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import { VueFeathers } from '@vue-feathers/vue-feathers'
-import { feathersClient } from './kadabra'
-
-Vue.use(VueFeathers, { feathersClient })
