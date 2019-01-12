0383e5f007d3d843fcd64fe50deeced5e9d9e30c
Patrick Keenan <sirberusio@gmail.com>
2019-01-11 21:51:35 -0800

    Update kadabra.js

--- admin/src/plugins/kadabra.js
+++ admin/src/plugins/kadabra.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
-import kadabraClient from '@kadabra/client'
+import client from '@kadabra/client'
 
-let url = window.location.href
+let kadabraServerUrl = window.location.href // When the admin is published, the client and server have the same url
 try {
-  if (webpackHotUpdate) {
-    url = 'localhost:7777'
+  if (webpackHotUpdate) {  // But when developing the admin, the admin itself runs on localhost:8080. So instead we
+    url = 'localhost:7777' // have to manually point to a local kadabra server, which always runs on localhost:7777
   }
 } catch(_) {}
 
-const service = kadabraClient(url)
-export const feathersClient = service('users').client
+const kadabra = client(kadabraServerUrl)
 
-Vue.prototype.$K = service
+Vue.prototype.$K = kadabra
+
+// Code below is a hack, will update @kadabra/client soon to absolve the hack
+export const feathersClient = service('users').client
 Vue.prototype.$kadabra = feathersClient
